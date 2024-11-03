4eb2d31f8da79b34067cde514797de49cbb4df94
bissanalmiari <bissanalmiari@gmail.com>
2024-11-03 12:41:19 +0200

    workout update

---
+++ workouts/workouts.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('.container').forEach(container => {
+    container.addEventListener('click', () => {
+        document.querySelectorAll('.container').forEach(c => c.classList.remove('active'));
+        container.classList.add('active');
+    });
+});

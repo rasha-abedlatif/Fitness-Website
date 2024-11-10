8700551113567e6edbe589fb7d75447dea32bed8
User <rashanasser279@gmail.com>
2024-11-10 12:04:56 +0200

    i did responsiveness for almost everything still there is few updates i wanna do on the home and it is finished

--- home/home.js
+++ home/home.js
@@ -40,12 +40,12 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     window.addEventListener("scroll", animateOnScroll);
-    animateOnScroll(); // Run once in case elements are already in view
+    animateOnScroll(); 
 });
 
 window.addEventListener("scroll", function() {
     let header = document.querySelector(".work-btn");
-    if (window.scrollY > 50) { // Adjust scroll value as needed
+    if (window.scrollY > 50) { 
       header.classList.add("scrolled");
     } else {
       header.classList.remove("scrolled");
@@ -85,6 +85,27 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   });
   
-//effect on brand slider:
-let copy = document.querySelector('.brand-slide').cloneNode(true);
-document.querySelector('.brands').appendChild(copy);
+//effect on brand slider [doing double images]:
+document.addEventListener("DOMContentLoaded", function() {
+  let copy = document.querySelector('.brand-slide').cloneNode(true);
+  document.querySelector('.brands').appendChild(copy);
+});
+
+
+//effect on the FAQS:
+let faqs = document.querySelectorAll(".faq");
+faqs.forEach(faq => {
+  faq.addEventListener("click", () => {
+    if (faq.classList.contains("active")) {
+      faq.classList.remove("active");
+    } else {
+      faq.classList.add("active");
+      faqs.forEach(otherFaq => {
+        if (otherFaq != faq) {
+          otherFaq.classList.remove("active");
+        }
+      });
+    }
+  });
+});
+

ea018aa1acade3755a345d33ce2827f0722a9b23
bissanalmiari <bissanalmiari@gmail.com>
2024-11-17 21:23:11 +0200

    .

---
+++ about us/aboutus.js
@@ -0,0 +1,8 @@
+window.addEventListener("scroll",function(){
+    let navbar=document.querySelector('header');
+    if(this.window.scrollY>50){
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+});

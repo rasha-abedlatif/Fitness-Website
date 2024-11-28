ba31d90ecd943afffd569509e7e1e38d1542dbfe
bissanalmiari <bissanalmiari@gmail.com>
2024-11-28 21:20:09 +0200

    .

--- js/aboutus.js
+++ js/aboutus.js
@@ -7,37 +7,12 @@ window.addEventListener("scroll",function(){
         header.classList.remove("scrolled");
     }
 });
-let currentSlide = 0;
-const slides = document.querySelectorAll('.slide');
-const slideInterval = 3000; // 3 seconds
 
-// Function to display the current slide and hide others
-function showSlide(index) {
-  slides.forEach((slide, i) => {
-    if (i === index) {
-      slide.classList.add('active'); // Show current slide
-    } else {
-      slide.classList.remove('active'); // Hide other slides
-    }
-  });
-}
+let backgroundContent = document.querySelector('.background-content');
+backgroundContent.classList.add('active');
+  let card = document.querySelector(".card");
 
-// Function to go to the next slide
-function nextSlide() {
-  currentSlide = (currentSlide + 1) % slides.length; // Loop back to the first slide
-  showSlide(currentSlide);
-}
-
-// Initial display
-showSlide(currentSlide);
-
-// Set interval to change slides automatically
-setInterval(nextSlide, slideInterval);
-document.addEventListener("DOMContentLoaded", () => {
-  const card = document.querySelector(".card");
-
-  // Intersection Observer to detect when .card is in the viewport
-  const observer = new IntersectionObserver(entries => {
+  let observer = new IntersectionObserver(entries => {
       entries.forEach(entry => {
           if (entry.isIntersecting) {
               card.classList.add("slide-in");
@@ -46,12 +21,9 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   observer.observe(card);
-});
-// Select all belief buttons and content items
-const buttons = document.querySelectorAll('.belief-button');
-const contentItems = document.querySelectorAll('.belief-item');
+let buttons = document.querySelectorAll('.belief-button');
+let contentItems = document.querySelectorAll('.belief-item');
 
-// Add event listeners to each button
 buttons.forEach((button) => {
   button.addEventListener('click', () => {
     // Remove 'active' class from all buttons and items

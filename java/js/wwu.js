9d55c9c9c0c6d47476d65cf913622ae1298a39b6
User <rashanasser279@gmail.com>
2024-11-30 22:16:57 +0200

    linked the food in the home page to the ones in the recipes page

--- java/js/wwu.js
+++ java/js/wwu.js
@@ -1,49 +1,53 @@
-
-document.querySelectorAll('.container').forEach(container => {
-  container.addEventListener('click', () => {
-      document.querySelectorAll('.container').forEach(c => c.classList.remove('active'));
-      container.classList.add('active');
+document.querySelectorAll(".container").forEach((container) => {
+  container.addEventListener("click", () => {
+    document
+      .querySelectorAll(".container")
+      .forEach((c) => c.classList.remove("active"));
+    container.classList.add("active");
   });
 });
 
-$(document).ready(function() {
+$(document).ready(function () {
+  function checkScroll() {
+    let scrollPosition = $(window).scrollTop();
+    let windowHeight = $(window).height();
 
-    function checkScroll() {
-       
-        let scrollPosition = $(window).scrollTop();
-        let windowHeight = $(window).height();
+    let firstImagePosition = $("#firstimg").offset().top;
+    let secondImagePosition = $("#secondimg").offset().top;
 
-        let firstImagePosition = $('#firstimg').offset().top;
-        let secondImagePosition = $('#secondimg').offset().top;
-
-        if (scrollPosition + windowHeight > firstImagePosition) {
-            $('#firstimg').addClass('visible'); 
-        }
-
-        if (scrollPosition + windowHeight > secondImagePosition) {
-            $('#secondimg').addClass('visible');  
-        }
+    if (scrollPosition + windowHeight > firstImagePosition) {
+      $("#firstimg").addClass("visible");
     }
 
+    if (scrollPosition + windowHeight > secondImagePosition) {
+      $("#secondimg").addClass("visible");
+    }
+  }
+
+  checkScroll();
+
+  $(window).on("scroll", function () {
     checkScroll();
-
-    $(window).on('scroll', function() {
-        checkScroll();
-    });
-
-
-    $('.btn-div').click(function() {
-      $('.inputs-div input, .inputs-div textarea'  ).val('');
-
-      $('.inputs-div select').prop('selectedIndex', 0);
-
-      $(".inputs-div input, .inputs-div select, .inputs-div textarea").removeClass("focused");
-    });
-
-    $(".inputs-div input, .inputs-div select, .inputs-div textarea").on("focus", function () {
-        $(".inputs-div input, .inputs-div select, .inputs-div textarea").removeClass("focused");
-        
-        $(this).addClass("focused");
-    });
-
   });
+
+  $(".btn-div").click(function () {
+    $(".inputs-div input, .inputs-div textarea").val("");
+
+    $(".inputs-div select").prop("selectedIndex", 0);
+
+    $(
+      ".inputs-div input, .inputs-div select, .inputs-div textarea"
+    ).removeClass("focused");
+  });
+
+  $(".inputs-div input, .inputs-div select, .inputs-div textarea").on(
+    "focus",
+    function () {
+      $(
+        ".inputs-div input, .inputs-div select, .inputs-div textarea"
+      ).removeClass("focused");
+
+      $(this).addClass("focused");
+    }
+  );
+});
